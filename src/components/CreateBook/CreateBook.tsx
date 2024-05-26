c79b54ec00e22de6726df02f5d041db943ad4313
lucabrlu <luca.breisinger@web.de>
2024-05-26 18:41:17 +0200

    Added preliminary GraphQl functionality for Create-Book

--- src/components/CreateBook/CreateBook.tsx
+++ src/components/CreateBook/CreateBook.tsx
@@ -1,3 +1,4 @@
+import { Button, Form } from 'react-bootstrap';
 import { useFieldArray, useForm } from 'react-hook-form';
 // eslint-disable-next-line no-lone-blocks
 {
@@ -12,18 +13,29 @@ import { Preis } from './Preis/Preis';
 import { Rabatt } from './Rabatt/Rabatt';
 import { Rating } from './Rating/Rating';
 import { Schlagwoerter } from './Schlagwoerter/Schlagwoerter';
+import { StatusModal } from './StatusModal/StatusModal';
+import { type SubmitHandler } from 'react-hook-form';
 import Table from 'react-bootstrap/Table';
 import { Titel } from './Titel/Titel';
+import { UPDATE_MUTATION } from '../EditBook/BookForm/mutations';
+import { id } from 'date-fns/locale';
+import { useMutation } from '@apollo/client';
+import { useState } from 'react';
 
 export interface Abbildungen {
     beschriftung: string;
     contentType: string;
 }
 
+export interface Titel {
+    titel: string;
+    untertitel: string;
+}
+
 export interface FormValues {
     version: string;
-    titel: string;
-    rating: number;
+    titel: Titel;
+    rating: string;
     art: 'KINDLE' | 'DRUCKAUSGABE';
     preis: number;
     rabatt: number;
@@ -34,13 +46,64 @@ export interface FormValues {
     schlagwoerter: string[];
 }
 
+const RABATT_TEILER = 100;
+
 /* eslint-disable max-lines-per-function */
 export const CreateInput = () => {
+    const [createBook] = useMutation(UPDATE_MUTATION);
+    const [createMessage, setCreateMessage] = useState({
+        visible: false,
+        nachricht: 'N/A',
+        error: false,
+    });
+
+    const CreateBook: SubmitHandler<FormValues> = (bookData) => {
+        createBook({
+            variables: {
+                id,
+                version: bookData.version,
+                isbn: bookData.isbn,
+                rating: Number.parseInt(bookData.rating, 10),
+                art: bookData.art,
+                preis: bookData.preis,
+                rabatt: bookData.rabatt / RABATT_TEILER,
+                lieferbar: bookData.lieferbar,
+                datum: bookData.datum,
+                homepage: bookData.homepage,
+                schlagwoerter: bookData.schlagwoerter,
+            },
+        })
+            .then(() =>
+                setCreateMessage({
+                    visible: true,
+                    nachricht: 'Das Aktualisieren war erfolgreich.',
+                    error: false,
+                }),
+            )
+            .catch((err) => {
+                if (err instanceof Error) {
+                    console.log(err);
+                    setCreateMessage({
+                        visible: true,
+                        nachricht: `Fehler: ${err.message}`,
+                        error: true,
+                    });
+                } else {
+                    setCreateMessage({
+                        visible: true,
+                        nachricht: 'Ein unbekannter Fehler ist aufgetreten.',
+                        error: true,
+                    });
+                }
+            });
+    };
+
     const {
         control,
         watch,
         register,
         unregister,
+        handleSubmit,
         formState: { errors },
     } = useForm<FormValues>();
     const {
@@ -67,73 +130,85 @@ export const CreateInput = () => {
 
     return (
         <>
-            <Table striped bordered style={{ tableLayout: 'fixed' }}>
-                <thead />
-                <tbody>
-                    <tr>
-                        <th>
-                            <Titel register={register} errors={errors} />
-                        </th>
-                        <th>
-                            <Isbn register={register} errors={errors} />
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>
-                            <Rating
-                                register={register}
-                                watch={watch}
-                                errors={errors}
-                            />
-                        </th>
-                        <th>
-                            <Buchart register={register} errors={errors} />
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>
-                            <Preis register={register} errors={errors} />
-                        </th>
-                        <th>
-                            <Rabatt register={register} errors={errors} />
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>
-                            <Homepage register={register} errors={errors} />
-                        </th>
-                        <th>
-                            <Datum register={register} errors={errors} />
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>
-                            <Lieferbar register={register} />
-                        </th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>
-                            <Schlagwoerter
-                                register={register}
-                                unregister={unregister}
-                                fields={schlagwoerterFields}
-                                append={schlagwoerterAppend}
-                                remove={schlagwoerterRemove}
-                            />
-                        </th>
-                        <th>
-                            {/* <Abbildungen
+            <Form onSubmit={handleSubmit(CreateBook)}>
+                <Table striped bordered style={{ tableLayout: 'fixed' }}>
+                    <thead />
+                    <tbody>
+                        <tr>
+                            <th>
+                                <Titel register={register} errors={errors} />
+                            </th>
+                            <th>
+                                <Isbn register={register} errors={errors} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <Rating
+                                    register={register}
+                                    watch={watch}
+                                    errors={errors}
+                                />
+                            </th>
+                            <th>
+                                <Buchart register={register} errors={errors} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <Preis register={register} errors={errors} />
+                            </th>
+                            <th>
+                                <Rabatt register={register} errors={errors} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <Homepage register={register} errors={errors} />
+                            </th>
+                            <th>
+                                <Datum register={register} errors={errors} />
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <Lieferbar register={register} />
+                            </th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <Schlagwoerter
+                                    register={register}
+                                    unregister={unregister}
+                                    fields={schlagwoerterFields}
+                                    append={schlagwoerterAppend}
+                                    remove={schlagwoerterRemove}
+                                />
+                            </th>
+                            <th>
+                                {/* <Abbildungen
                                 register={register}
                                 unregister={unregister}
                                 fields={abbildungenFields}
                                 append={abbildungenAppend}
                                 remove={abbildungenRemove}
                             /> */}
-                        </th>
-                    </tr>
-                </tbody>
-            </Table>
+                            </th>
+                        </tr>
+                    </tbody>
+                </Table>
+                <Button
+                    variant="primary"
+                    type="submit"
+                    className="w-100 d-flex justify-content-center"
+                >
+                    Speichern
+                </Button>
+            </Form>
+            {createMessage.visible && (
+                <StatusModal createMessage={createMessage} />
+            )}
         </>
     );
 };
